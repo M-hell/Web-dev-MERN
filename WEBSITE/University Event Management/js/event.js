49fe30ee303417ce7ccb4bf25b2497d5ddb27b44
Samrat Sarkar <sarkarsamrat16112004@gmail.com>
2023-11-14 00:35:13 +0530

    Create event.js

---
+++ WEBSITE/University Event Management/js/event.js
@@ -0,0 +1,15 @@
+gsap.from("#mymaindiv #mysecondarymaindiv",{
+     opacity:0,
+     y:80,
+     duration:1   
+})
+gsap.from("#mymaindiv #mythirdmaindiv",{
+    opacity:0,
+    y:-80,
+    duration:1   
+})
+gsap.from("#mymaindiv #myfourthmaindiv",{
+    opacity:0,
+    x:80,
+    duration:1   
+})
